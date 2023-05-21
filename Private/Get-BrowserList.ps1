1b8f121e8a560bf5394697fcf1968b3e7be82eff
Alain Herve <alain.1337dev@outlook.com>
2023-05-21 19:06:59 +0200

    Update Get-BrowserList.ps1

--- Private/Get-BrowserList.ps1
+++ Private/Get-BrowserList.ps1
@@ -1,7 +1,7 @@
 function Get-BrowserList {
     <#
     .SYNOPSIS
-        Lists of all web browsers in the market – the popular, as well as the little-known ones. 
+        Lists of all web browsers in the market – the popular, as well as the little-known ones.
     .DESCRIPTION
         A longer description of the function, its purpose, common use cases, etc.
     .NOTES
@@ -15,22 +15,13 @@ function Get-BrowserList {
         Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
     #>
     [CmdletBinding()]
-    [OutputType([System.Collections.Generic.List[psobject]])]
+    [OutputType([System.Collections.Generic.List[BrowserInfo]])]
     param ()
 
     process {
-        $BrowserList = [System.Collections.Generic.List[psobject]]::new()
-        $links = $(Invoke-WebRequest 'https://www.webdevelopersnotes.com/browsers-list').Links | Where-Object { $_.rel -eq "noopener noreferrer" -and !$_.title.EndsWith("web site") }
-        $links = $links.Where({ ![string]::IsNullOrWhiteSpace($_.href) }) | ForEach-Object {
-            $title, $href = ($_.title -replace " download link$", ''), $_.href -replace "^https?://", '//' -replace "^//?", 'https://'
-            [PSCustomObject]@{
-                title = $title
-                href  = $href
-            }
-        } | Sort-Object -Property href -Unique
-        $links.ForEach({ [void]$BrowserList.Add($_) })
+        # Todo: Create BrowserList API.
+        $BrowserList = [BrowserManager]::GetBrowserList()
     }
-
     end {
         return $BrowserList
     }
